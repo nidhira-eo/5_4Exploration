ee2ac302420cbcfa52417b67f49fa41994c2bfd7
Nidhira Palakolanu <hcps-palakolan@henricostudents.org>
2026-02-05 11:57:41 -0500

    first commit

---
+++ init_db.py
@@ -0,0 +1,44 @@
+import os
+import psycopg2
+
+# Connect to database
+conn = psycopg2.connect(
+    host = "drhscit.org",
+    database = os.environ['DB'],
+    user = os.environ['DB_UN'],
+    password = os.environ['DB_PW']
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Create a new table for reviews
+cur.execute('DROP TABLE IF EXISTS reviews;')
+cur.execute('CREATE TABLE reviews (name VARCHAR (150) NOT NULL,'
+                                 'review TEXT);'
+                                )
+# From Exploration: Insert data
+cur.execute('INSERT INTO reviews(name, review)'
+            'VALUES(%s,%s)',
+            ('Fox Mulder', 'Loved the pizza!')
+            )
+
+cur.execute('INSERT INTO reviews(name, review)'
+            'VALUES(%s,%s)',
+            ('Xavier File', 'Great dessert choices!'
+             ))
+# Commit the transaction to save the changes
+conn.commit()
+
+
+# From exploration: Select data
+cur.execute('SELECT * FROM reviews WHERE name = %s',('Fox Mulder',))
+results = cur.fetchall()
+print(results)
+
+
+
+
+#Close the cursor and connection
+cur.close()
+conn.close()
